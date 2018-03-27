84b7574a57aa37f031e22d9a6d0f89380f2b70d4
s9iper1 <s9iper1@gmail.com>
2018-03-27 12:33:29 +0500

    fix location

--- app/src/main/java/com/inf/os/fileup/MainActivity.java
+++ app/src/main/java/com/inf/os/fileup/MainActivity.java
@@ -3,6 +3,7 @@ package com.inf.os.fileup;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -15,11 +16,15 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.GeolocationPermissions;
+import android.webkit.JsResult;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -34,7 +39,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    WebView webView;
+
+    private WebView webView;
     private static final String TAG = MainActivity.class.getSimpleName();
     private String mCM;
     private ValueCallback<Uri> mUM;
@@ -57,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
                     Manifest.permission.CAMERA,
                     Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
-
         webView = (WebView) findViewById(R.id.ifView);
         webView.setWebChromeClient(new WebChromeClient() {
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
@@ -72,6 +77,13 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setAppCacheEnabled(true);
         webSettings.setDatabaseEnabled(true);
         webSettings.setAllowFileAccess(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setGeolocationDatabasePath(getFilesDir().getPath());
+        
+
 
         if (Build.VERSION.SDK_INT >= 21) {
             webSettings.setMixedContentMode(0);
@@ -84,6 +96,22 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new Callback());
         webView.loadUrl("http://ciceroni.biz/");
         webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                AlertDialog.Builder alertBldr = new AlertDialog.Builder(MainActivity.this);
+                alertBldr.setMessage(message);
+                alertBldr.setTitle("Alert");
+                alertBldr.show();
+                result.confirm();
+                return true;
+            }
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+
             //For Android 3.0+
             public void openFileChooser(ValueCallback<Uri> uploadMsg) {
                 mUM = uploadMsg;
@@ -137,6 +165,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressWarnings("deprecation")
+    public static void clearCookies(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            Log.d(TAG, "Using clearCookies code for API >=" + String.valueOf(Build.VERSION_CODES.LOLLIPOP_MR1));
+            CookieManager.getInstance().removeAllCookies(null);
+            CookieManager.getInstance().flush();
+        } else {
+            Log.d(TAG, "Using clearCookies code for API <" + String.valueOf(Build.VERSION_CODES.LOLLIPOP_MR1));
+            CookieSyncManager cookieSyncMngr= CookieSyncManager.createInstance(context);
+            cookieSyncMngr.startSync();
+            CookieManager cookieManager= CookieManager.getInstance();
+            cookieManager.removeAllCookie();
+            cookieManager.removeSessionCookie();
+            cookieSyncMngr.stopSync();
+            cookieSyncMngr.sync();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
@@ -177,6 +223,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i("TAG", "url : " + url);
             if (url.startsWith("tel:")) {
                 Intent intent = new Intent(Intent.ACTION_DIAL,
                         Uri.parse(url));
